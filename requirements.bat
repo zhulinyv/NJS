5bfa9703b211eaa74d1fe6f343309db5f3ed1e40
zhulinyv <zhulinyv2005@outlook.com>
2023-03-12 13:42:28 +0800

    🛠️ 更新依赖

--- requirements.bat
+++ requirements.bat
@@ -82,7 +82,7 @@ poetry run pip install nonebot-adapter-qqguild==0.1.4
 poetry run pip install nonebot-bison==0.5.5
 poetry run pip install nonebot-plugin-apscheduler==0.2.0
 poetry run pip install nonebot-plugin-chatrecorder==0.2.1
-poetry run pip install nonebot-plugin-datastore==0.4.0
+poetry run pip install nonebot-plugin-datastore==0.5.1
 poetry run pip install nonebot-plugin-easy-translate==0.1.3
 poetry run pip install nonebot-plugin-gocqhttp==0.6.8
 poetry run pip install nonebot-plugin-guild-patch==0.2.2

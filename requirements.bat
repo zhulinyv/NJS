dd14aeffac64e2a1a4e04941393ebd9f0acb418e
zhulinyv <zhulinyv2005@outlook.com>
2023-03-15 16:59:58 +0800

    🛠️ 更新了依赖

--- requirements.bat
+++ requirements.bat
@@ -37,7 +37,7 @@ poetry run pip install cycler==0.11.0
 poetry run pip install dateparser==1.1.1
 poetry run pip install dnspython==2.2.1
 poetry run pip install ecdsa==0.18.0
-poetry run pip install EdgeGPT==0.0.51
+poetry run pip install EdgeGPT==0.0.56.2
 poetry run pip install emoji==2.1.0
 poetry run pip install et-xmlfile==1.1.0
 poetry run pip install expandvars==0.9.0

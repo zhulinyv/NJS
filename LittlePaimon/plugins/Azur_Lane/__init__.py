78eb99ef5bb94c6c93b03a1f8036f21faf9e1efa
zhulinyv <zhulinyv2005@outlook.com>
2023-04-29 17:21:28 +0800

    :bulb: 碧蓝航线添加注释

--- LittlePaimon/plugins/Azur_Lane/__init__.py
+++ LittlePaimon/plugins/Azur_Lane/__init__.py
@@ -26,7 +26,10 @@ __plugin_meta__ = PluginMetadata(
     }
 )
 
-screenshot_cmd = on_command('碧蓝航线wiki', priority=10, block=True, rule=Rule(permission_check), state={
+screenshot_cmd = on_command(
+    '碧蓝航线wiki',
+    aliases={'blhx wiki', '碧蓝航线', 'al wiki', 'az', 'blhx'},
+    priority=10, block=True, rule=Rule(permission_check), state={
     'pm_name':        '碧蓝航线wiki',
     'pm_description': '截图获取碧蓝任意角色武器或其它模块的wiki',
     'pm_usage':       '碧蓝航线wiki<角色名|武器名|其它>',
@@ -35,14 +38,15 @@ screenshot_cmd = on_command('碧蓝航线wiki', priority=10, block=True, rule=Ru
 
 
 @screenshot_cmd.handle()
-async def _(event: MessageEvent, msg: Message = CommandArg()):
-    url1 = 'https://wiki.biligame.com/blhx/'
-    url2 = msg.extract_plain_text().strip()
-    url = url1 + url2
+async def _(msg: Message = CommandArg()):
+    url = 'https://wiki.biligame.com/blhx/'
+    item = msg.extract_plain_text().strip()
+    url += item
+    # 转换链接格式
     url = quote(url, safe = string.printable)
-    await screenshot_cmd.send(f'正在尝试获取wiki，请稍等...更多信息请访问原文{url}')
+    await screenshot_cmd.send(f'正在尝试获取wiki, 请稍等...更多信息请访问原文捏~{url}')
     try:
         img = await blhx_screenshot(url)
         await screenshot_cmd.send(MessageSegment.image(img))
     except Exception:
-        await screenshot_cmd.send('wiki获取失败，无法访问该网页，请稍候再试')
+        await screenshot_cmd.send('wiki获取失败, 无法访问该网页, 请稍候再试')

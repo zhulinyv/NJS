9096dcb9f5a86e29301cc527e39829ee744577bc
zhulinyv <zhulinyv2005@outlook.com>
2023-05-06 23:55:11 +0800

    :children_crossing: 修改默认配置

--- LittlePaimon/config/config/model.py
+++ LittlePaimon/config/config/model.py
@@ -11,29 +11,29 @@ class ConfigModel(BaseModel):
     img_use_cache: bool = Field(True, alias='图片资源缓存开关')
     reboot_card_ban: List[int] = Field([], alias='禁用重启时修改群名片群列表')
 
-    sim_gacha_cd_group: int = Field(30, alias='模拟抽卡群冷却')
-    sim_gacha_cd_member: int = Field(60, alias='模拟抽卡群员冷却')
-    sim_gacha_max: int = Field(5, alias='模拟抽卡单次最多十连数')
+    sim_gacha_cd_group: int = Field(3, alias='模拟抽卡群冷却')
+    sim_gacha_cd_member: int = Field(90, alias='模拟抽卡群员冷却')
+    sim_gacha_max: int = Field(9, alias='模拟抽卡单次最多十连数')
 
     auto_myb_enable: bool = Field(True, alias='米游币自动获取开关')
     auto_myb_hour: int = Field(8, alias='米游币开始执行时间(小时)')
     auto_myb_minute: int = Field(0, alias='米游币开始执行时间(分钟)')
 
     auto_sign_enable: bool = Field(False, alias='米游社自动签到开关')
-    auto_sign_hour: int = Field(0, alias='米游社签到开始时间(小时)')
-    auto_sign_minute: int = Field(5, alias='米游社签到开始时间(分钟)')
+    auto_sign_hour: int = Field(7, alias='米游社签到开始时间(小时)')
+    auto_sign_minute: int = Field(0, alias='米游社签到开始时间(分钟)')
 
     ssbq_enable: bool = Field(True, alias='实时便签检查开关')
     ssbq_begin: int = Field(0, alias='实时便签停止检查开始时间')
     ssbq_end: int = Field(6, alias='实时便签停止检查结束时间')
-    ssbq_check: int = Field(16, alias='实时便签检查间隔')
+    ssbq_check: int = Field(30, alias='实时便签检查间隔')
 
     ys_auto_update: int = Field(24, alias='ys自动更新小时')
     ysa_auto_update: int = Field(24, alias='ysa自动更新小时')
-    ysd_auto_update: int = Field(6, alias='ysd自动更新小时')
+    ysd_auto_update: int = Field(12, alias='ysd自动更新小时')
 
     cloud_genshin_enable: bool = Field(True, alias='云原神自动签到开关')
-    cloud_genshin_hour: int = Field(7, alias='云原神签到时间(小时)')
+    cloud_genshin_hour: int = Field(6, alias='云原神签到时间(小时)')
 
     request_event: bool = Field(True, alias='启用好友和群请求通知')
     auto_add_friend: bool = Field(False, alias='自动接受好友请求')
@@ -42,7 +42,7 @@ class ConfigModel(BaseModel):
 
     screenshot_enable: bool = Field(False, alias='启用网页截图权限')
 
-    guess_voice_time: int = Field(30, alias='原神猜语音时间')
+    guess_voice_time: int = Field(60, alias='原神猜语音时间')
 
     admin_enable: bool = Field(True, alias='启用Web端')
     admin_password: str = Field('admin', alias='Web端管理员密码')

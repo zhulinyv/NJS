dd18b3323ddc0d24a30d5c028fd3e4edabbaa490
zhulinyv <zhulinyv2005@outlook.com>
2023-05-13 07:33:30 +0800

    :ambulance: 风控后重试

--- src/plugins/njs_help/__init__.py
+++ src/plugins/njs_help/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from nonebot import require
 from nonebot import on_command
 from nonebot.params import CommandArg
+from nonebot.exception import ActionFailed
 from nonebot.adapters.onebot.v11 import (
     Bot,
     Event,
@@ -62,13 +63,31 @@ async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
         try:
             await njs_help.finish('指令如下: \n' + help_reply[f"h{number}_r"], at_sender=True)
         except KeyError:
-            if int(number) <= 99:
-                if number in ["1", "14", "58", "76"]:
-                    help_image = Path(os.path.join(os.path.dirname(__file__), "resource")) / f"h{number}.jpg"
+            try:
+                if int(number) <= 99:
+                    if number in ["1", "14", "58", "76"]:
+                        help_image = Path(os.path.join(os.path.dirname(__file__), "resource")) / f"h{number}.jpg"
+                    else:
+                        help_image = Path(os.path.join(os.path.dirname(__file__), "resource")) / f"h{number}.png"
+                    await njs_help.finish('指令如下: \n' + MessageSegment.image(help_image), at_sender=True)
                 else:
-                    help_image = Path(os.path.join(os.path.dirname(__file__), "resource")) / f"h{number}.png"
-                await njs_help.finish('指令如下: \n' + MessageSegment.image(help_image), at_sender=True)
-            else:
-                await njs_help.finish('功能扩建中...', at_sender=True)
+                    await njs_help.finish('功能扩建中...', at_sender=True)
+            except ActionFailed:
+                if isinstance(event, GroupMessageEvent):
+                    msgs = []
+                    message_list = []
+                    message_list.append('指令如下: \n' + MessageSegment.image(help_image))
+                    for msg in message_list:
+                        msgs.append({
+                            'type': 'node',
+                            'data': {
+                                'name': f"{NICKNAME}",
+                                'uin': bot.self_id,
+                                'content': msg
+                            }
+                        })
+                    await bot.call_api('send_group_forward_msg', group_id=event.group_id, messages=msgs)
+                else:
+                    await njs_help.finish("指令发不出来啦~! 请查看帮助文档:\n优先: zhulinyv.github.io/NJS\n备用: cnblogs.com/xytpz/p/NJS.html", at_sender=True)
     else:
         await njs_help.finish('功能扩建中...', at_sender=True)

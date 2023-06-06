a04b4b70383b88fad0f6c090fa2b8a2c7576c50f
zhulinyv <zhulinyv2005@outlook.com>
2023-06-06 20:56:14 +0800

    :sparkles: 更新复读插件

--- src/plugins/nonebot_plugin_repeater/__init__.py
+++ src/plugins/nonebot_plugin_repeater/__init__.py
@@ -1,19 +1,22 @@
+import re
+
 from nonebot import on_message, logger
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+
 from . import config
-import re
 
 repeater_group = config.repeater_group
 shortest = config.shortest_length
+blacklist = config.blacklist
 
-m = on_message(priority=100, block=False)
+m = on_message(priority=10, block=False)
 
 last_message = {}
 message_times = {}
 
 
 # 消息预处理
-def messagePreprocess(message: str):
+def message_preprocess(message: str):
     raw_message = message
     contained_images = {}
     images = re.findall(r'\[CQ:image.*?]', message)
@@ -26,19 +29,23 @@ def messagePreprocess(message: str):
 
 @m.handle()
 async def repeater(bot: Bot, event: GroupMessageEvent):
+    # 检查是否在黑名单中
+    if event.raw_message in blacklist:
+        logger.debug(f'[复读姬] 检测到黑名单消息: {event.raw_message}')
+        return
     gid = str(event.group_id)
     if gid in repeater_group or "all" in repeater_group:
         global last_message, message_times
-        message, raw_message = messagePreprocess(str(event.message))
-        logger.debug(f'这一次消息: {message}')
-        logger.debug(f'上一次消息: {last_message.get(gid)}')
+        message, raw_message = message_preprocess(str(event.message))
+        logger.debug(f'[复读姬] 这一次消息: {message}')
+        logger.debug(f'[复读姬] 上一次消息: {last_message.get(gid)}')
         if last_message.get(gid) != message:
             message_times[gid] = 1
         else:
             message_times[gid] += 1
-        logger.debug(f'已重复次数: {message_times.get(gid)}/{config.shortest_times}')
+        logger.debug(f'[复读姬] 已重复次数: {message_times.get(gid)}/{config.shortest_times}')
         if message_times.get(gid) == config.shortest_times:
-            logger.debug(f'原始的消息: {str(event.message)}')
-            logger.debug(f"欲发送信息: {raw_message}")
+            logger.debug(f'[复读姬] 原始的消息: {str(event.message)}')
+            logger.debug(f"[复读姬] 欲发送信息: {raw_message}")
             await bot.send_group_msg(group_id=event.group_id, message=raw_message, auto_escape=False)
         last_message[gid] = message
--- src/plugins/nonebot_plugin_repeater/config.py
+++ src/plugins/nonebot_plugin_repeater/config.py
@@ -8,7 +8,10 @@ if 'repeater_min_message_length' not in config:
     logger.warning('[复读姬] 未发现配置项 `repeater_min_message_length` , 采用默认值: 1')
 if 'repeater_min_message_times' not in config:
     logger.warning('[复读姬] 未发现配置项 `repeater_min_message_times` , 采用默认值: 2')
+if 'repeater_blacklist' not in config:
+    logger.warning('[复读姬] 未发现配置项 `repeater_blacklist` , 采用默认值: []')
 
 repeater_group = config.get('repeater_group', [])
 shortest_length = config.get('repeater_min_message_length', 1)
 shortest_times = config.get('repeater_min_message_times', 2)
+blacklist = config.get('repeater_blacklist', [])

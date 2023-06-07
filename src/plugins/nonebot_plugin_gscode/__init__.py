7cf3345b099a1e0e107be48cd19b03bcb3ced184
zhulinyv <zhulinyv2005@outlook.com>
2023-06-07 20:19:50 +0800

    :sparkles: 更新兑换码插件

--- src/plugins/nonebot_plugin_gscode/__init__.py
+++ src/plugins/nonebot_plugin_gscode/__init__.py
@@ -1,19 +1,35 @@
-from nonebot.adapters.onebot.v11 import Bot
-from nonebot.adapters.onebot.v11.event import GroupMessageEvent, MessageEvent
-from nonebot.plugin import on_command
 from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot
+from nonebot.internal.matcher import Matcher
+from nonebot.plugin import PluginMetadata, on_command
+from nonebot.adapters.onebot.v11.event import MessageEvent, GroupMessageEvent
 
-from .data_source import getCodes
+from .data_source import get_msg
 
-codeMatcher = on_command("gscode", aliases={"兑换码"}, priority=5, block=True)
+__plugin_meta__ = PluginMetadata(
+    name="GsCode",
+    description="查询原神/星穹铁道前瞻直播兑换码",
+    usage="""查询原神/星穹铁道前瞻直播兑换码
+注意：经测试，兑换码接口返回与前瞻直播有 2 分钟左右延迟，应为正常现象，请耐心等待。
+/gscode
+/srcode""",
+)
+
+gs_code_matcher = on_command("gscode", aliases={"原神兑换码"}, priority=5)
+sr_code_matcher = on_command("srcode", aliases={"铁道兑换码", "星穹铁道兑换码"}, priority=5)
 
 
-@codeMatcher.handle()
-async def _(bot: Bot, event: MessageEvent, state: T_State):
-    if event.get_plaintext() not in ["gscode", "兑换码"]:
-        await codeMatcher.finish()
-    codes = await getCodes()
+@gs_code_matcher.handle()
+@sr_code_matcher.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State, matcher: Matcher):
+    if str(state["_prefix"]["command_arg"]):
+        await matcher.finish()
+    codes = (
+        await get_msg("gs")
+        if isinstance(matcher, gs_code_matcher)
+        else await get_msg("sr")
+    )
     if isinstance(event, GroupMessageEvent):
-        await bot.send_group_forward_msg(group_id=event.group_id, messages=codes)  # type: ignore
+        await bot.send_group_forward_msg(group_id=event.group_id, messages=codes)
     else:
-        await bot.send_private_forward_msg(user_id=event.user_id, messages=codes)  # type: ignore
+        await bot.send_private_forward_msg(user_id=event.user_id, messages=codes)
--- src/plugins/nonebot_plugin_gscode/data_source.py
+++ src/plugins/nonebot_plugin_gscode/data_source.py
@@ -1,40 +1,58 @@
 import json
-from re import findall
-from time import time, strftime, localtime
-from typing import Dict, List
+from time import time
+from re import sub, findall
+from typing import Dict, List, Union
+from datetime import datetime, timezone, timedelta
 
 from httpx import AsyncClient
-from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot.log import logger
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+
+TZ = timezone(timedelta(hours=8))
 
 
-async def getData(type, data: Dict = {}) -> Dict:
+async def get_data(type, mhy_type: str = "", data: Dict = {}) -> Dict:
     """米哈游接口请求"""
 
+    uid = 288909600 if mhy_type == "sr" else 75276550
     url = {
-        "index": f"https://api-takumi.mihoyo.com/event/bbslive/index?act_id={data.get('actId', '')}",
-        "mi18n": f"https://webstatic.mihoyo.com/admin/mi18n/bbs_cn/{data.get('mi18n', '')}/{data.get('mi18n', '')}-zh-cn.json",
-        "code": f"https://webstatic.mihoyo.com/bbslive/code/{data.get('actId', '')}.json?version=1&time={int(time())}",
-        "actId": "https://bbs-api.mihoyo.com/painter/api/user_instant/list?offset=0&size=20&uid=75276550",  # 原神米游姬
+        "act_id": f"https://bbs-api.mihoyo.com/painter/api"
+        f"/user_instant/list?offset=0&size=20&uid={uid}",
+        "index": "https://api-takumi.mihoyo.com/event/miyolive/index",
+        "code": "https://api-takumi-static.mihoyo.com/event/miyolive/refreshCode",
     }
     async with AsyncClient() as client:
         try:
-            res = await client.get(url[type])
+            if type == "index":
+                res = await client.get(
+                    url[type], headers={"x-rpc-act_id": data.get("actId", "")}
+                )
+            elif type == "code":
+                res = await client.get(
+                    url[type],
+                    params={
+                        "version": data.get("version", ""),
+                        "time": f"{int(time())}",
+                    },
+                    headers={"x-rpc-act_id": data.get("actId", "")},
+                )
+            else:
+                res = await client.get(url[type])
             return res.json()
         except Exception as e:
             logger.opt(exception=e).error(f"{type} 接口请求错误")
             return {"error": f"[{e.__class__.__name__}] {type} 接口请求错误"}
 
 
-async def getActId() -> str:
+async def get_act_id(mhy_type) -> str:
     """获取 ``act_id``"""
 
-    ret = await getData("actId")
+    ret = await get_data(type="act_id", mhy_type=mhy_type)
     if ret.get("error") or ret.get("retcode") != 0:
         return ""
 
-    actId = ""
-    keywords = ["来看《原神》", "版本前瞻特别节目"]
+    act_id = ""
+    keywords = ["版本前瞻特别节目"]
     for p in ret["data"]["list"]:
         post = p.get("post", {}).get("post", {})
         if not post:
@@ -44,95 +62,140 @@ async def getActId() -> str:
         shit = json.loads(post["structured_content"])
         for segment in shit:
             link = segment.get("attributes", {}).get("link", "")
-            if "观看直播" in segment.get("insert", "") and link:
-                matched = findall(r"act_id=(\d{8}ys\d{4})", link)
+            if "直播" in segment.get("insert", "") and link:
+                matched = findall(r"act_id=(.*?)\&", link)
                 if matched:
-                    actId = matched[0]
-        if actId:
+                    act_id = matched[0]
+        if act_id:
             break
 
-    return actId
+    return act_id
 
 
-async def getCodes() -> List[MessageSegment]:
+async def get_live_data(act_id: str) -> Dict:
+    """获取直播数据，尤其是 ``code_ver``"""
+
+    ret = await get_data(type="index", data={"actId": act_id})
+    if ret.get("error") or ret.get("retcode") != 0:
+        return {"error": ret.get("error") or "前瞻直播数据异常"}
+
+    live_data_raw = ret["data"]["live"]
+    live_template = json.loads(ret["data"]["template"])
+    live_data = {
+        "code_ver": live_data_raw["code_ver"],
+        "title": live_data_raw["title"].replace("特别节目", ""),
+        "header": live_template["kvDesktop"],
+        "room": live_template["liveConfig"][0]["desktop"],
+    }
+    if live_data_raw["is_end"]:
+        live_data["review"] = live_template["reviewUrl"]["args"]["post_id"]
+    else:
+        now = datetime.fromtimestamp(time(), TZ)
+        start = datetime.strptime(live_data_raw["start"], "%Y-%m-%d %H:%M:%S").replace(
+            tzinfo=TZ
+        )
+        if now < start:
+            live_data["start"] = live_data_raw["start"]
+
+    return live_data
+
+
+async def get_codes(version: str, act_id: str) -> Union[Dict, List[Dict]]:
+    """获取兑换码"""
+
+    ret = await get_data(type="code", data={"version": version, "actId": act_id})
+    if ret.get("error") or ret.get("retcode") != 0:
+        return {"error": ret.get("error") or "兑换码数据异常"}
+
+    codes_data = []
+    for code_info in ret["data"]["code_list"]:
+        codes_data.append(
+            {
+                "items": sub("<.*?>", "", code_info["title"].replace("&nbsp;", " ")),
+                "code": code_info["code"],
+            }
+        )
+
+    return codes_data
+
+
+async def get_msg(mhy_type) -> List[MessageSegment]:
     """生成最新前瞻直播兑换码合并转发消息"""
 
-    actId = await getActId()
-    if not actId:
+    act_id = await get_act_id(mhy_type=mhy_type)
+    nickname = "原神前瞻直播" if mhy_type == "gs" else "星穹铁道前瞻直播"
+    if not act_id:
         return [
             MessageSegment.node_custom(
                 user_id=2854196320,
-                nickname="原神前瞻直播",
+                nickname=nickname,
                 content=Message(MessageSegment.text("暂无前瞻直播资讯！")),
             )
         ]
 
-    indexRes = await getData("index", {"actId": actId})
-    if not indexRes.get("data") or not indexRes["data"].get("mi18n", ""):
+    live_data = await get_live_data(act_id)
+    if live_data.get("error"):
         return [
             MessageSegment.node_custom(
                 user_id=2854196320,
-                nickname="原神前瞻直播",
-                content=Message(
-                    MessageSegment.text(indexRes.get("message") or "没有找到 mi18n！")
-                ),
+                nickname=nickname,
+                content=Message(MessageSegment.text(live_data["error"])),
             )
         ]
-    mi18n = indexRes["data"].get("mi18n", "")
-
-    mi18nRes = await getData("mi18n", {"mi18n": mi18n})
-    codeRes = await getData("code", {"actId": actId})
-    nickname = mi18nRes.get("act-title", "").replace("特别节目", "") or "原神前瞻直播"
-    if indexRes["data"].get("remain", 0) or not len(codeRes):
+    elif live_data.get("start"):
         return [
             MessageSegment.node_custom(
                 user_id=2854196320,
-                nickname=nickname,
-                content=Message(MessageSegment.image(mi18nRes["pc-kv"])),
+                nickname=live_data["title"],
+                content=Message(MessageSegment.image(live_data["header"])),
             ),
             MessageSegment.node_custom(
                 user_id=2854196320,
-                nickname=nickname,
-                content=Message(
-                    MessageSegment.text(
-                        f"预计第一个兑换码发放时间为 "
-                        f"{strftime('%m 月 %d 日 %H:%M', localtime(int(mi18nRes['time1'])))}"
-                        "\n\n* 官方接口数据有 2 分钟左右延迟，请耐心等待下~"
-                    )
-                ),
+                nickname=live_data["title"],
+                content=Message(MessageSegment.text(live_data["room"])),
             ),
         ]
 
-    nextCode = (
-        ""
-        if len(codeRes) == 3
-        else (
-            "，下一个兑换码发放时间为 "
-            + strftime("%H:%M:%S", localtime(int(mi18nRes[f"time{len(codeRes) + 1}"])))
-        )
-    )
-    codes = [
+    codes_data = await get_codes(live_data["code_ver"], act_id)
+    if isinstance(codes_data, Dict):
+        return [
+            MessageSegment.node_custom(
+                user_id=2854196320,
+                nickname=live_data["title"],
+                content=Message(MessageSegment.text(codes_data["error"])),
+            )
+        ]
+    codes_msg = [
         MessageSegment.node_custom(
             user_id=2854196320,
-            nickname=nickname,
+            nickname=live_data["title"],
             content=Message(
                 MessageSegment.text(
-                    f"当前发放了 {len(codeRes)} 个兑换码{nextCode}\n{mi18nRes['exchange-tips']}"
+                    f"当前发布了 {len(codes_data)} 个兑换码，请在有效期内及时兑换哦~"
+                    + "\n\n* 官方接口数据有 2 分钟左右延迟，请耐心等待下~"
                 )
             ),
-        )
-    ]
-    for codeInfo in codeRes:
-        gifts = findall(
-            r">\s*([\u4e00-\u9fa5]+|\*[0-9]+)\s*<",
-            codeInfo["title"].replace("&nbsp;", " "),
-        )
-        codes.append(
+        ),
+        *[
             MessageSegment.node_custom(
                 user_id=2854196320,
-                nickname="+".join(g for g in gifts if not g[-1].isdigit()) or nickname,
-                content=Message(MessageSegment.text(codeInfo["code"])),
+                nickname=code["items"] or live_data["title"],
+                content=Message(MessageSegment.text(code["code"])),
+            )
+            for code in codes_data
+        ],
+    ]
+    if live_data.get("review"):
+        codes_msg.append(
+            MessageSegment.node_custom(
+                user_id=2854196320,
+                nickname=live_data["title"],
+                content=Message(
+                    MessageSegment.text(
+                        "直播已经结束，查看回放：\n\n"
+                        + f"https://www.miyoushe.com/ys/article/{live_data['review']}"
+                    )
+                ),
             )
         )
-
-    return codes
+    return codes_msg

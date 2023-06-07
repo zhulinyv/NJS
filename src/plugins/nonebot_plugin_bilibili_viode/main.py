3a9d976f482a696100fc28f50ed0b0fd74b968a3
zhulinyv <zhulinyv2005@outlook.com>
2023-06-07 23:13:27 +0800

    :bug: 修复B站卡片分享所有关键词报错

--- src/plugins/nonebot_plugin_bilibili_viode/main.py
+++ src/plugins/nonebot_plugin_bilibili_viode/main.py
@@ -15,12 +15,19 @@ share_sort_url = on_message(block=False)
 
 @share_sort_url.handle()
 async def _(event: Union[MessageEvent, GuildMessageEvent], bot: Bot):
+    # print(repr(event.raw_message), event.raw_message)
     video_id = bilibili_video_id_validate(event.raw_message)
+    # print(video_id)
     if not video_id:
-        video_id = await bilibili_video_id_from_url(event.raw_message)
-    if not video_id:
-        return
-    elif video_id.startswith("BV"):
+        # print(">>>>>")
+        try:
+            video_id = await bilibili_video_id_from_url(event.raw_message)
+        except:
+            return
+        # print(video_id)
+    else:
+        pass
+    if video_id.startswith("BV"):
         video_info = await VideoInfo.get(bvid=video_id)
     elif video_id.startswith("av"):
         video_info = await VideoInfo.get(aid=video_id[2:])

b2baa0f52d2c530fd454fdbc9d08ddfc8105a3b6
zhulinyv <zhulinyv2005@outlook.com>
2023-07-22 19:20:42 +0800

    :zap: 缩短冷却时间

--- src/plugins/nonebot_plugin_impact/utils.py
+++ src/plugins/nonebot_plugin_impact/utils.py
@@ -58,13 +58,19 @@ class Utils:
                 self.write_ejaculation_data()  # 这种结构, 不想重构也不想新建json了，所以放宽了这么多{"user_id": {"date": {"ejaculation": 123}}}
         self.usage = """指令1: 嗦牛子 (给目标牛牛增加长度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
 指令2: 打胶 | 开导 (给自己牛牛增加长度)
-指令3: pk | 对决 (普通的pk,单纯的random实现输赢, 胜利方获取败方随机数/2的牛牛长度)
-指令4: 查询 (目标牛牛长度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
+指令3: 牛子pk | 牛子对决 (普通的pk,单纯的random实现输赢, 胜利方获取败方随机数/2的牛牛长度)
+指令4: 牛子查询 (目标牛牛长度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
 指令5: jj排行榜 | jj排名 | jj榜单 | jjrank (字面意思, 输出倒数五位和前五位, 以及自己的排名)
 指令6: 开启淫趴|禁止淫趴 (由管理员 | 群主 | SUPERUSERS开启或者关闭淫趴)
-指令7: 日群友|透群友|日群主|透群主|日管理|透管理  (字面意思, 当使用透群友的时候如果at了人那么直接指定)
+指令7: 日群友|透群友|日群主|透群主|日管理|透管理 (字面意思, 当使用透群友的时候如果at了人那么直接指定)
 指令8: 注入查询 | 摄入查询 (查询目标被透注入的量，后接(历史|全部), 可查看总被摄入的量, 无艾特的时候是自己, 有at的时候是目标)
-指令9: 淫趴介绍 | 淫趴说明| 淫趴帮助 (输出淫趴插件的命令列表)"""
+指令9: 淫趴介绍 | 淫趴说明| 淫趴帮助 (输出淫趴插件的命令列表)
+
+指令1: 扣 (给目标小学增加深度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
+指令2: 扣扣 (给自己小学增加深度)
+指令3:小学pk | 小学对决 (普通的pk,单纯的random实现输赢, 胜利方获取败方随机数/2的小学长度)
+指令4: 小学查询 (目标小学长度, 自己或者他人, 通过艾特选择对象, 没有at时目标是自己)
+指令5: xx排行榜 | xx排名 | xx榜单 | xxrank (字面意思, 输出倒数五位和前五位, 以及自己的排名)"""
         self.not_allow = '群内还未开启淫趴游戏, 请管理员或群主发送"开启淫趴", "禁止淫趴"以开启/关闭该功能'  # 未开启该功能的send信息
         self.jj_variable = ["牛子", "牛牛", "丁丁", "JJ"]  # JJ变量
         self.xx_variable = ["小学", "小雪", "穴穴", "BB"]
@@ -73,10 +79,10 @@ class Utils:
         self.suo_cd_data = {}  # 嗦牛子冷却数据
         self.ejaculation_cd = {}  # 射精CD
         config = nonebot.get_driver().config  # 获取配置
-        self.dj_cd_time: int = getattr(config, "djcdtime", 300)  # 打胶冷却时间
-        self.pk_cd_time: int = getattr(config, "pkcdtime", 60)  # pk冷却时间
-        self.suo_cd_time: int = getattr(config, "suocdtime", 300)  # 嗦牛子冷却时间
-        self.fuck_cd_time: int = getattr(config, "fuckcdtime", 3600)  # 透群友冷却时间
+        self.dj_cd_time: int = getattr(config, "djcdtime", 10)  # 打胶冷却时间
+        self.pk_cd_time: int = getattr(config, "pkcdtime", 10)  # pk冷却时间
+        self.suo_cd_time: int = getattr(config, "suocdtime", 10)  # 嗦牛子冷却时间
+        self.fuck_cd_time: int = getattr(config, "fuckcdtime", 10)  # 透群友冷却时间
 
     def write_group_data(self) -> None:
         """写入群配置"""
